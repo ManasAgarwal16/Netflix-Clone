15c2b7dfd02d75691034467edb43da3751c59d9c
= <=>
2021-07-25 16:42:25 +0530

    complete user functionality in  Backend

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -28,5 +28,66 @@ const updateUser = async (req, res) => {
     res.status(403).json('You can update only your account!');
   }
 };
+const deleteUser = async (req, res) => {
+  if (req.user.id === req.params.id || req.user.isAdmin) {
+    try {
+      await User.findByIdAndDelete(req.params.id);
+      res.status(200).json('User has been deleted...');
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json('You can delete only your account!');
+  }
+};
 
-export { updateUser };
+const getUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, ...info } = user._doc;
+    res.status(200).json(info);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+const getAllUser = async (req, res) => {
+  const query = req.query.new;
+  if (req.user.isAdmin) {
+    try {
+      const users = query
+        ? await User.find().sort({ _id: -1 }).limit(5)
+        : await User.find();
+      res.status(200).json(users);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json('You are not allowed to see all users!');
+  }
+};
+
+const getUserStats = async (req, res) => {
+  const today = new Date();
+  const latYear = today.setFullYear(today.setFullYear() - 1);
+
+  try {
+    const data = await User.aggregate([
+      {
+        $project: {
+          month: { $month: '$createdAt' },
+        },
+      },
+      {
+        $group: {
+          _id: '$month',
+          total: { $sum: 1 },
+        },
+      },
+    ]);
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+export { updateUser, deleteUser, getUser, getAllUser, getUserStats };
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,7 +1,18 @@
 import express from 'express';
 const router = express.Router();
-import { updateUser } from '../controllers/userController.js';
+import {
+  updateUser,
+  deleteUser,
+  getUser,
+  getAllUser,
+  getUserStats,
+} from '../controllers/userController.js';
 import { verifyToken } from '../middleware/verifyToken.js';
 
 router.put('/:id', verifyToken, updateUser);
+router.delete('/:id', verifyToken, deleteUser);
+router.get('/find/:id', getUser);
+router.get('/', verifyToken, getAllUser);
+router.get('/stats', verifyToken, getUserStats);
+
 export default router;
